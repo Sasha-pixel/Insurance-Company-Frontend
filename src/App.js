0d238e3c8acde3bf6d762947122ee3ad2f731a91
Alexander Kanenkov <al.kanenckoff@yandex.ru>
2022-04-23 02:39:46 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import Header from "./components/common/header/Header";
 import Content from "./components/common/content/Content";
 import Footer from "./components/common/footer/Footer";
 import {BrowserRouter} from "react-router-dom";
+import {PageLayout} from "./components/common/header/PageLayout";
+import { AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
+import ProfileContent from "./components/common/header/ProfileContent";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <Header/>
+                <PageLayout>
+                    <AuthenticatedTemplate>
+                        <ProfileContent />
+                    </AuthenticatedTemplate>
+                    <UnauthenticatedTemplate>
+                        <p>You are not signed in! Please sign in.</p>
+                    </UnauthenticatedTemplate>
+                </PageLayout>
                 <Content greeting='Hello, my dear friend!'/>
                 <Footer/>
             </BrowserRouter>
---
+++ src/components/common/header/graph.js
@@ -0,0 +1,21 @@
+/**
+ * Attaches a given access token to a Microsoft Graph API call. Returns information about the user
+ */
+import {graphConfig} from "../../../authConfig";
+
+
+export async function callMsGraph(accessToken) {
+    const headers = new Headers();
+    const bearer = `Bearer ${accessToken}`;
+
+    headers.append("Authorization", bearer);
+
+    const options = {
+        method: "GET",
+        headers: headers
+    };
+
+    return fetch(graphConfig.graphMeEndpoint, options)
+        .then(response => response.json())
+        .catch(error => console.log(error));
+}
